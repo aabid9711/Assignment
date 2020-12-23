b91373ddb4ffb9731285b7419aefa84794eedc4c
aabid9711 <51543343+aabid9711@users.noreply.github.com>
2020-12-24 01:04:29 +0530

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,16 @@
+<Script>
+
+
+    // published date get from the id object given in the css file  //
+    var x = document.getElementById('published').innerHTML;
+
+    console.log(x); // it will print the published date on console //
+
+    var y=document.getElementById('object').innerHTML;
+
+    console.log(y) //print the value of object on console//
+
+    var z = document.getElementById('bidding').innerHTML;
+
+    console.log(z);
+</Script>
